beafe95a1c56f22de1ff01715a3a3c353880a288
Jordan Schroter <jordan.schroter@gmail.com>
2016-08-01 17:48:53 -0400

    improved proptypes

---
+++ src/formStoreShape.js
@@ -0,0 +1,17 @@
+import { PropTypes } from 'react'
+
+export default PropTypes.shape({
+  subscribe: PropTypes.func.isRequired,
+  getState: PropTypes.func.isRequired,
+  getFormState: PropTypes.func.isRequired,
+  dispatch: PropTypes.func.isRequired,
+  addValidator: PropTypes.func.isRequired,
+  removeValidator: PropTypes.func.isRequired,
+  addSubmitListener: PropTypes.func.isRequired,
+  removeSubmitListener: PropTypes.func.isRequired,
+  validate: PropTypes.func.isRequired,
+  submit: PropTypes.func.isRequired,
+  reset: PropTypes.func.isRequired,
+  clear: PropTypes.func.isRequired,
+  unsubscribe: PropTypes.func.isRequired
+})
