118e1c3f2da6b65262d6426ece3456b25c590295
Jordan Schroter <jordan.schroter@gmail.com>
2016-10-28 20:03:04 -0400

    use context.form for connectForm rather than props

--- src/connectForm.js
+++ src/connectForm.js
@@ -1,11 +1,22 @@
 import React from 'react'
 import { connect } from 'react-redux'
 
+import formStoreShape from './formStoreShape'
+
 export default function connectForm(...args) {
   return function wrapWithConnect(BaseComponent) {
     const ConnectedBaseComponent = connect(...args)(BaseComponent)
 
-    return props =>
-      <ConnectedBaseComponent {...props} store={props.form} /> // eslint-disable-line
+    function ConnectedComponent(props, context) {
+      return (
+        <ConnectedBaseComponent {...props} store={context.form} />
+      )
+    }
+
+    ConnectedComponent.contextTypes = {
+      theme: formStoreShape.isRequired
+    }
+
+    return ConnectedComponent
   }
 }
