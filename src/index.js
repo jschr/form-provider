d936fb10585234a4da76ebfeeb2271c30f50272e
Jordan Schroter <jordan.schroter@gmail.com>
2016-11-17 10:04:52 -0500

    version bump

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
+import * as actions from './actions'
+
+export { actions }
 export { default as constants } from './constants'
 export { default as enhancer } from './enhancer'
 export { default as reducer } from './reducer'
+
 export { default as connectForm } from './connectForm'
 export { default as FormProvider } from './FormProvider'
 export { default as Field } from './Field'
 export { default as withForm } from './withForm'
 
-// actions
-import * as actions from 'actions'
-export const actions
