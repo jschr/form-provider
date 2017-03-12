604dc8bc0a011efe71ce58d1d8af27793f87fbfb
Jordan Schroter <jordan.schroter@gmail.com>
2017-03-11 23:28:57 -0500

    missing setFormState in store shape

--- src/formStoreShape.ts
+++ src/formStoreShape.ts
@@ -4,6 +4,7 @@ export default PropTypes.shape({
   subscribe: PropTypes.func.isRequired,
   getState: PropTypes.func.isRequired,
   getFormState: PropTypes.func.isRequired,
+  setFormState: PropTypes.func.isRequired,
   dispatch: PropTypes.func.isRequired,
   addValidator: PropTypes.func.isRequired,
   removeValidator: PropTypes.func.isRequired,
